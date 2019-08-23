8d58ac76210e04c0fe0aac6e6cad68b51731177b
Trahan.hunter <Trahan.hunter@yahoo.com>
2019-08-23 11:43:11 -0500

    implemented armor system

--- inventorySystem/Inventory.cs
+++ inventorySystem/Inventory.cs
@@ -10,6 +10,8 @@ namespace inventorySystem
     {
         int damage = 5;
         int weapon = 0;
+        int defense = 5;
+        int armor = 0;
         float gold = 0.00f;
 
 
@@ -24,8 +26,10 @@ namespace inventorySystem
                 Console.WriteLine("0: Exit");
                 Console.WriteLine("1: Equip Weapon");
                 Console.WriteLine("2: Uneqip Weapon");
-                Console.WriteLine("3: Add Gold");
-                Console.WriteLine("4: Subtract Gold");
+                Console.WriteLine("3: Equip Armor");
+                Console.WriteLine("4: Unequip Armor");
+                Console.WriteLine("5: Add Gold");
+                Console.WriteLine("6: Subtract Gold");
 
                 //Get input
                 choice = Console.ReadLine();
@@ -41,15 +45,25 @@ namespace inventorySystem
                 {
                     unequipWeapon();
                 }
-                
+
                 else if (choice == "3")
+                {
+                    equipArmor();
+                }
+
+                else if (choice == "4")
+                {
+                    unequipArmor();
+                }
+
+                else if (choice == "5")
                 {
                     Console.Write("How much gold?");
                     float addedGold = Convert.ToSingle(Console.ReadLine());
                     addGold(addedGold);
                 }
 
-                else if (choice == "4")
+                else if (choice == "6")
                 {
                     Console.Write("How much gold?");
                     float addedGold = Convert.ToSingle(Console.ReadLine());
@@ -62,7 +76,7 @@ namespace inventorySystem
 
 
 
-
+        //weapon System
         public void equipWeapon()
         {
 
@@ -155,6 +169,98 @@ namespace inventorySystem
             
         }
 
+        //armor System
+        public void equipArmor()
+        {
+            string menu3 = "";
+
+            if (armor == 0)
+            {
+
+                while (menu3 != "0")
+                {
+                    Console.WriteLine("\nArmor list");
+                    Console.WriteLine("0: Back");
+                    Console.WriteLine("1: Leather");
+                    Console.WriteLine("2: Iron");
+                    Console.WriteLine("3: Steel");
+                    Console.WriteLine("4: Platnium");
+
+                    string choice2 = "";
+
+                    choice2 = Console.ReadLine();
+                    Console.WriteLine("");
+
+
+                    if (choice2 == "0")
+                    {
+                        menu3 = "0";
+                    }
+
+                    else if (choice2 == "1")
+                    {
+                        Console.WriteLine("Equipped Leather armor.");
+                        armor = 10;
+                        Console.WriteLine("Defense: " + armor);
+                        armor = 1;
+                        menu3 = "0";
+                    }
+
+                    else if (choice2 == "2")
+                    {
+                        Console.WriteLine("Equipped Iron armor.");
+                        armor = 15;
+                        Console.WriteLine("Defense: " + armor);
+                        armor = 1;
+                        menu3 = "0";
+                    }
+
+                    else if (choice2 == "3")
+                    {
+                        Console.WriteLine("Equipped Steel armor.");
+                        armor = 20;
+                        Console.WriteLine("Defense: " + armor);
+                        armor = 1;
+                        menu3 = "0";
+                    }
+
+                    else if (choice2 == "4")
+                    {
+                        Console.WriteLine("Equipped Platnium armor.");
+                        armor = 25;
+                        Console.WriteLine("Defense: " + armor);
+                        armor = 1;
+                        menu3 = "0";
+                    }
+
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("There is armor equipped");
+            }
+        }
+
+        public void unequipArmor()
+        {
+            if (armor == 1)
+            {
+                Console.WriteLine("Unequiped armor.");
+                armor = 5;
+                Console.WriteLine("defense: " + armor);
+                defense = 0;
+            }
+
+            else
+            {
+                Console.WriteLine("There is no armor equipped");
+            }
+
+        }
+
+        //gold System
         public void addGold(float amount)
         {
             Console.WriteLine("Got " + amount + " gold!");
