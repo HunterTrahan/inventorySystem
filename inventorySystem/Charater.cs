e7544414886e5f4763acd7f125c9f2e442106a60
Trahan.hunter <Trahan.hunter@yahoo.com>
2019-08-26 11:13:56 -0500

    Started Character system

---
+++ inventorySystem/Charater.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventorySystem
+{
+    class Charater
+    {
+        private string _name = "";
+        private int _xp = 0;
+        private int _level = 1;
+        private int[] _requiredEXP = { 100, 200, 600, 1000 };
+
+        public Charater(string name)
+        {
+            _name = name;
+        }
+
+        public string Name()
+        {
+            return _name;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_name);
+            Console.WriteLine("Level: " + _level);
+            Console.WriteLine("EXP: " + _xp);
+        }
+
+        //exp system
+        public int EXP
+        {
+            get
+            {
+                return _xp;
+            }
+
+            set
+            {
+                _xp = value;
+                Console.WriteLine(_name + " gained EXP and now has " + _xp);
+                if (_level <= _requiredEXP.Length)
+                {
+                    if (_xp >= _requiredEXP[_level - 1])
+                    {
+                        _level++;
+                        Console.WriteLine(_name + " 's leveled up to " + _level + "!");
+                    }
+                }
+                
+            }
+        }
+    }
+}
--- inventorySystem/Program.cs
+++ inventorySystem/Program.cs
@@ -10,8 +10,52 @@ namespace inventorySystem
     {
         static void Main(string[] args)
         {
-            Inventory inventory = new Inventory();
-            inventory.Menu();
+            //Inventory inventory = new Inventory();
+            //inventory.Menu();
+
+            Charater player = new Charater("Aigis");
+            player.Print();
+
+            Charater party1 = new Charater("Chie");
+            Charater party2 = new Charater("Futaba");
+            party1.Print();
+            party2.Print();
+
+            player.EXP = 30;
+            player.EXP = player.EXP = 50;
+            player.EXP++;
+            player.EXP += 40;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+            player.EXP += 200;
+
+
+            int[] testArray = new int[4];
+
+            testArray[0] = 1;
+            testArray[1] = 3;
+            testArray[2] = 5;
+            testArray[3] = 7;
+
+            int[] testArray2 = { 2, 4, 6, 8 };
+
+            string[] stringArray = new string[3];
+
+            Charater[] party = { player, party1, party2, new Charater("Lonk") };
+
+            Console.ReadKey();
         }
     }
 
